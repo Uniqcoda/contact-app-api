7ab9d2526690c0d629af54fcb1e5587b2f8d229e
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-19 00:47:23 +0100

    refactor the contact schema and also use typing for it

--- src/models/contacts.model.ts
+++ src/models/contacts.model.ts
@@ -1,5 +1,11 @@
 import mongoose from 'mongoose';
 
+export interface ICreateContact extends mongoose.Document {
+	firstName: string;
+	lastName?: string;
+	phone: string;
+	email?: string;
+}
 const Schema = mongoose.Schema;
 
 const contactSchema = new Schema({
@@ -7,11 +13,11 @@ const contactSchema = new Schema({
 	createdAt: String, // the ISO date of when the contact was created
 	isBlocked: Boolean,
 	value: {
-		firstName: String,
-		lastName: String,
-		phone: String,
-		email: String,
-	},
+	firstName: String,
+	lastName: String,
+	phone: String,
+	email: String,
+	isBlocked: { type: Boolean, default: false },
 });
 
-export const Contact = mongoose.model('Contact', contactSchema);
+export const Contact = mongoose.model<ICreateContact>('Contact', contactSchema);
