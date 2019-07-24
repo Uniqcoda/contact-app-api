e6b848ecbe552e557fe903eb727225418c9b3d2f
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-24 19:24:59 +0100

    add typings for creating a contact and fetching a single contact

---
+++ src/types/contactType.ts
@@ -0,0 +1,57 @@
+import { GraphQLObjectType,GraphQLInputObjectType, GraphQLString, GraphQLBoolean } from 'graphql';
+import { ICreateContact } from '../models/contacts.model';
+
+export const ContactType = new GraphQLObjectType<ICreateContact>({
+	name: 'Contact',
+	description: 'Information about a single contact',
+	fields: () => ({
+		id: {
+			type: GraphQLString,
+			description: 'The unique id of the contact',
+			resolve: data => data._id,
+		},
+		firstName: {
+			type: GraphQLString,
+			description: 'The first name of the contact',
+		},
+		lastName: {
+			type: GraphQLString,
+			description: 'The last name of the contact',
+		},
+		phone: {
+			type: GraphQLString,
+			description: 'The phone number of the contact',
+		},
+		email: {
+			type: GraphQLString,
+			description: 'The email address of the contact',
+		},
+		isBlocked: {
+			type: GraphQLBoolean,
+			description: 'True if the contact is blocked',
+		},
+	}),
+});
+
+export const ContactInput = new GraphQLInputObjectType({
+	name: 'ContactInput',
+	description: 'Information about creating a single contact',
+	fields: () => ({
+		firstName: {
+			type: GraphQLString,
+			description: 'The first name of the contact',
+		},
+		lastName: {
+			type: GraphQLString,
+			description: 'The last name of the contact',
+		},
+		phone: {
+			type: GraphQLString,
+			description: 'The phone number of the contact',
+		},
+		email: {
+			type: GraphQLString,
+			description: 'The email address of the contact',
+		},
+	}),
+});
