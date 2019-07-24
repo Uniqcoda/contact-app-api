84577d1ccc6b97fe17dda333f9e3e2a181bcee5f
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-24 19:26:25 +0100

    add graphql schema for query and mutation

--- src/graphqlSchema.ts
+++ src/graphqlSchema.ts
@@ -1,45 +1,58 @@
-import {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLSchema,
-  GraphQLList,
-} from 'graphql';
-import {getContacts, getBlockedContacts, getContact} from './controllers/index'
-import ContactType from './types/contact';
+import { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList } from 'graphql';
+import { getAllContacts, getBlockedContacts, getContactById, createContact } from './controllers/index';
+import { ContactType, ContactInput } from './types/contactType';
 
-const query = new GraphQLObjectType ({
-  name: 'Query',
-  description: 'The query root of Contactica App.',
-  fields: () => ({
-    contacts: {
-      type: new GraphQLList(ContactType),
-      description: 'All the unblocked contacts',
-      resolve: () => getContacts(),
-    },
-    blockedContacts: {
-      type: new GraphQLList(ContactType),
-      description: 'All the blocked contacts',
-      resolve: () => getBlockedContacts(),
-    },
-    contact: {
-      type: ContactType,
-      description: 'Info about a single contact',
-      args: {
-        contactId: {
-          type: GraphQLString,
-          description: 'The ID of the contact to fetch',
-        },
-      },
-      resolve: (_, { contactId }) => {
-        return getContact(contactId);
-      }
-    }
-  }
-  )
-})
+const query = new GraphQLObjectType({
+	name: 'ContacticaApp',
+	description: 'The query root of Contactica App.',
+	fields: () => ({
+		contacts: {
+			type: new GraphQLList(ContactType),
+			description: 'All the unblocked contacts',
+			resolve: () => getAllContacts(),
+		},
+		blockedContacts: {
+			type: new GraphQLList(ContactType),
+			description: 'All the blocked contacts',
+			resolve: () => getBlockedContacts(),
+		},
+		contact: {
+			type: ContactType,
+			description: 'Info about a single contact',
+			args: {
+				id: {
+					type: GraphQLString,
+					description: 'The ID of the contact to fetch',
+				},
+			},
+			resolve: (_, { id }) => {
+				return getContactById(id);
+			},
+		},
+	}),
+});
+
+const mutation = new GraphQLObjectType({
+	name: 'ContactMutations',
+	description: 'All the mutations supported by Contactica App.',
+	fields: () => ({
+		createContact: {
+			type: ContactType,
+			description: 'Create a new contact',
+			args: {
+				input: {
+					type: ContactInput,
+					description: 'The values to create a new contact',
+				},
+			},
+			resolve: (_, args) => createContact(args.input),
+		},
+	}),
+});
 
 const schema = new GraphQLSchema({
-  query,
+	query,
+	mutation,
 });
 
 export default schema;
