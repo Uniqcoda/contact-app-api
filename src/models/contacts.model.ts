90ac2e1c90263a6d52f1845ccf062204f31dcf5a
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-24 19:19:52 +0100

    add contact schema and unique validator for email and phone

--- src/models/contacts.model.ts
+++ src/models/contacts.model.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
 
 export interface ICreateContact extends mongoose.Document {
 	firstName: string;
@@ -11,9 +12,10 @@ const Schema = mongoose.Schema;
 const contactSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	phone: String,
-	email: String,
+	phone: { type: String, index: true, unique: true, required: true },
+	email: { type: String, index: true, unique: true},
 	isBlocked: { type: Boolean, default: false },
 });
 
+contactSchema.plugin(uniqueValidator, {message: 'is already in your contact.'})
 export const Contact = mongoose.model<ICreateContact>('Contact', contactSchema);
