3e95a853bb699fcd20c2d9f8733cbfd8a2c308c2
Mary Etokwudo <etokwudomary@gmail.com>
2019-07-25 23:43:52 +0100

    edit the menu bar to add icon

--- client/src/components/MenuBar/MenuBar.js
+++ client/src/components/MenuBar/MenuBar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import { IoMdPersonAdd } from 'react-icons/io';
 
 export default function MenuBar() {
 	return (
 		<>
 			<Navbar bg='primary' variant='dark'>
 				<Navbar.Brand as={Link} to='/'>
-					Contactica
+					<IoMdPersonAdd/>Contactica
 				</Navbar.Brand>
 				<Nav className='mr-auto'>
 					<Nav.Link as={Link} to='/'>
-						All Contacts
+						All
 					</Nav.Link>
 					<Nav.Link as={Link} to='/blocked-contacts'>
-						Blocked Contacts
+						Blocked
 					</Nav.Link>
 				</Nav>
 			</Navbar>
